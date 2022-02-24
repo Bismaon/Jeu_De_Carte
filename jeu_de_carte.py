b16f7a56a2efc3bd6c0910b572090f7e36999c13
Bismaon <64341829+Bismaon@users.noreply.github.com>
2022-02-24 20:12:54 +0700

    updated jeu_de_carte.py

--- jeu_de_carte.py
+++ jeu_de_carte.py
@@ -4,10 +4,10 @@ from bataille import Bataille
 
 def main():
     paquet_a=PaquetDeCarte()
-    paquet_a=PaquetDeCarte()
+    paquet_b=PaquetDeCarte()
     paquet_a.remplir()
-    paquet_a.remplir()
-    BlackJack(paquet_a, paquet_a)
+    paquet_b.remplir()
+    BlackJack(paquet_a, paquet_b)
 
 if __name__=="__main__":
     main()
