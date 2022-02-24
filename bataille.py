e2821f001fdb74db3454d90780b41cad8f0b7221
Bismaon <64341829+Bismaon@users.noreply.github.com>
2022-02-24 20:59:14 +0700

    updated bataille.py and deleted trialing spaces

--- bataille.py
+++ bataille.py
@@ -1,10 +1,17 @@
 from random import shuffle
-from carte import *
+from carte import Carte, PaquetDeCarte
 ###To do list:
 #    - make it visual
 #    - have interaction with the user
 #    - be able to choose which game one wnats to play, black, or bataille
 class Bataille:
+    """La class Batialle permet de:
+        -   jouer à la bataille contre "l'ordinateur"
+        -   de tirer des cartes
+        -   de savoir combien de carte reste dans chaque paquet
+        -   finir au bout de 100 retour de carte, et de voir qui a gagné!
+    """
+
     def __init__(self, paquet1:list, paquet2:list):
         self.jeu1=paquet1
         self.jeu2=paquet2
@@ -15,56 +22,68 @@ class Bataille:
             - Les cartes sont tirer en meme temps
             - trefles < carreau < coeur < pique"""
         v_couleur={'trefle':1, 'carreau':2, 'coeur':3, 'pique':4}
-        j1=0
-        j2=0
+        indice_j1=0
+        indice_j2=0
         tour=0
-        shuffle(self.jeu1)
-        shuffle(self.jeu2)
+        shuffle(self.jeu1.contenu)
+        shuffle(self.jeu2.contenu)
         while len(self.jeu1.contenu)!=0 and len(self.jeu2.contenu)!=0:
             tour+=1
-            if j1>=len(self.jeu1.contenu):
-                j1=0
-            if j2>=len(self.jeu2.contenu):
-                j2=0
-            carte1=self.jeu1.getCarteAt(j1)
-            carte2=self.jeu2.getCarteAt(j2)
-            input(f'{carte1.getNom()} de {carte1.getCouleur()} contre {carte2.getNom()} de {carte2.getCouleur()}')
-            if carte1.Valeur>carte2.Valeur:
-                input(f"J1: {carte1.getNom()} de {carte1.getCouleur()} l'emporte!")
+            if indice_j1>=len(self.jeu1.contenu):
+                indice_j1=0
+            if indice_j2>=len(self.jeu2.contenu):
+                indice_j2=0
+            carte1=self.jeu1.get_carte_at(indice_j1)
+            carte2=self.jeu2.get_carte_at(indice_j2)
+            input('{}: {} de {} contre {} de {}'.format(tour,
+                                                        carte1.get_nom(),
+                                                        carte1.get_couleur(),
+                                                        carte2.get_nom(),
+                                                        carte2.get_couleur()))
+            if carte1.valeur>carte2.valeur:
+                input(f"J1: {carte1.get_nom()} de {carte1.get_couleur()} l'emporte!")
                 self.jeu2.contenu.append(carte1)
-                self.jeu1.contenu.pop(j1)
-                j2+=1
-            elif carte1.Valeur == carte2.Valeur:
-                if v_couleur[carte1.getCouleur()]>v_couleur[carte2.getCouleur()]:
-                    input(f"J1: {carte1.getNom()} de {carte1.getCouleur()} l'emporte!")
+                self.jeu1.contenu.pop(indice_j1)
+                indice_j2+=1
+            elif carte1.valeur == carte2.valeur:
+                if v_couleur[carte1.get_couleur()]>v_couleur[carte2.get_couleur()]:
+                    input(f"J1: {carte1.get_nom()} de {carte1.get_couleur()} l'emporte!")
                     self.jeu2.contenu.append(carte1)
-                    self.jeu1.contenu.pop(j1)
-                    j2+=1
-                elif v_couleur[carte1.getCouleur()]==v_couleur[carte2.getCouleur()]:
+                    self.jeu1.contenu.pop(indice_j1)
+                    indice_j2+=1
+                elif v_couleur[carte1.get_couleur()]==v_couleur[carte2.get_couleur()]:
                     input("Egalité!")
-                    j1+=1
-                    j2+=1
+                    indice_j1+=1
+                    indice_j2+=1
                 else:
-                    input(f"J2: {carte2.getNom()} de {carte2.getCouleur()} l'emporte!")
+                    input(f"J2: {carte2.get_nom()} de {carte2.get_couleur()} l'emporte!")
                     self.jeu1.contenu.append(carte2)
-                    self.jeu2.contenu.pop(j2)
-                    j1+=1
+                    self.jeu2.contenu.pop(indice_j2)
+                    indice_j1+=1
             else:
-                input(f"J2: {carte2.getNom()} de {carte2.getCouleur()} l'emporte!")
+                input(f"J2: {carte2.get_nom()} de {carte2.get_couleur()} l'emporte!")
                 self.jeu1.contenu.append(carte2)
-                self.jeu2.contenu.pop(j2)
-                j1+=1
+                self.jeu2.contenu.pop(indice_j2)
+                indice_j1+=1
             if tour>100:
                 if len(self.jeu2.contenu)>len(self.jeu1.contenu):
-                    print("Joueur Numero 2 a gagné!", len(self.jeu2.contenu))
+                    print(f"""Joueur Numero 2 a gagné! avec {len(self.jeu2.contenu)} cartes restantes!\n
+                              Le joueur numero 1 avait {len(self.jeu1.contenu)} cartes restantes!""")
                     return
-                elif len(self.jeu1.contenu)==len(self.jeu2.contenu):
+                if len(self.jeu1.contenu)==len(self.jeu2.contenu):
                     print("Egalité!")
                     return
                 else:
-                    print("Joueur Numero 1 a gagné!", len(self.jeu1.contenu))
+                    print(f"""Joueur Numero 1 a gagné! avec {len(self.jeu1.contenu)} cartes restantes!\n
+                              Le joueur numero 2 avait {len(self.jeu2.contenu)} cartes restantes!""")
                     return
         if len(self.jeu2.contenu)==0:
             print("Joueur Numero 1 a gagné!", tour)
         else:
             print("Joueur Numero 2 a gagné!", tour)
+paquet_a=PaquetDeCarte()
+paquet_b=PaquetDeCarte()
+paquet_a.remplir()
+paquet_b.remplir()
+bt=Bataille(paquet_a, paquet_b)
+bt.bataille()
